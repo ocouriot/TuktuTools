be14710ff6a024d3a93cdacfb378e960ca2d0360
ocouriot <o.couriot@gmail.com>
2022-11-10 11:20:17 -0500

    data documentation

--- R/datadocumentation.R
+++ R/datadocumentation.R
@@ -1,19 +1,16 @@
-#' Example of movement data for 4 Barren-ground caribou individuals
-#'
-#' 
+#' Movement data of barren-ground caribou
 #'
 #' @usage 
-#' data(barrenground)
+#' data(caribou)
 #' 
-#' @format data frame containing the ID, the time of the location, the Lon and Lat coordinates (WGS84), 
-#' the x and y coordinates (in Lambert Conformal Conic as: "+proj=lcc +lat_1=50 +lat_2=70 +lat_0=65 +lon_0=-120 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+#' @name caribou
+#' 
+#' @format data frame with the name of the individuals (ID), the sex, the coordinates of the GPS locations (Lon,Lat in WGS84 and x,y in Canada Lambert Conformal Conic) and date and time (Time) of the GPD locations
 #' 
 #' @examples 
-#' data(barrenground)
-#' 
+#' data(caribou)
+#' head(caribou)
 #'
 #' @source GNWT
 #' @keywords data
-"barrenground"
-
-
+"caribou"
