83bf7c01433e5a9763223225a217e92845de6289
Elie Gurarie <egurarie@umd.edu>
2022-11-10 11:46:07 -0500

    Caribou data

--- R/datadocumentation.R
+++ R/datadocumentation.R
@@ -5,12 +5,21 @@
 #' 
 #' @name caribou
 #' 
-#' @format data frame with the name of the individuals (ID), the sex, the coordinates of the GPS locations (Lon,Lat in WGS84 and x,y in Canada Lambert Conformal Conic) and date and time (Time) of the GPD locations
+#' @format data frame with the following columns: 
+#' #' \describe{
+#'   \item{id}{ID of animal}
+#'   \item{start}{Date of beginning of collaring}
+#'   \item{end}{Date of death or censoring}
+#'   \item{fate}{One of "dead", or "censored"}
+#' }
 #' 
-#' @examples 
-#' data(caribou)
-#' head(caribou)
+#' name of 4 individuals (ID), the sex, the coordinates 
+#' of the GPS locations (Lon,Lat in WGS84 and x,y in Canada Lambert Conformal Conic) 
+#' and date and time (Time) of the GPS locations. These are real data courtesy of NWT
+#' but anonymized and shifted in time, longitude and latitude. 
+#' 
+#' @examples examples/example_caribou.R
 #'
-#' @source GNWT
+#' @source GNWT Department of Environment and Natural Resources
 #' @keywords data
 "caribou"
