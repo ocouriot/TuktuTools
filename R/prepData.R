df80743e1919144be27bdfd9e865b82fc51ee081
ocouriot <o.couriot@gmail.com>
2022-12-29 13:27:56 -0500

    changes in prepData to be restrictive oe not

--- R/prepData.R
+++ R/prepData.R
@@ -12,8 +12,8 @@
 #' Time: date and time vector of the relocation (of class POSIXct)
 #' @param start starting date of the period of interest as a character in the form "mm-dd"
 #' (example for the 19th of May: "05-19")
-#' @param end end date of the period of interest as a character in the form "mm-dd"
-#' (example for the 7th of July: "07-07")
+#' @param end end date of the period of interest as a character in the form "mm-dd" (excluded)
+#' (example for the 8th of July: "07-08")
 #' @param nfixes individuals with fewer than nfixes observations per day over the period of of interest are removed
 #' @param dayloss maximum number of consecutive days with missing locations
 #' (for example, if an individual has loss signal for more than 3 consecutive days, 
@@ -25,30 +25,32 @@
 #' 
 #' @export
 
-prepData <- function(df, start, end, nfixes = Inf, dayloss = Inf){
+prepData <- function(df, start, end, nfixes = Inf, dayloss = Inf, restrictive = FALSE){
 
     # just keep time series between defined start and end
   tempo <- df %>% as.data.frame %>% 
     mutate(Year = year(Time), 
            start = ymd(paste(Year, start), tz = tz(Time)),
            end = ymd(paste(Year, end), tz = tz(Time))) %>%
-    subset(Time >= start & Time <= end) %>% 
+    subset(Time >= start & Time < end) %>% 
     mutate(start = as.Date(start), end = as.Date(end))
   
   # Remove individuals for which monitoring stopped before the end or 
   # began after the start
-  
-  tempo2 <- tempo %>% ddply(c("ID", "Year"), function(x) x %>% 
-                              mutate(start.monitoring = as.Date(min(.$Time)), 
-                                     end.monitoring = as.Date(max(.$Time)))) %>% 
-    subset(start.monitoring == start & end.monitoring == end) %>% 
-    mutate(ID_Year = as.factor(paste(ID, Year, sep ="_")))
+  if(restrictive){
+    tempo <- tempo %>% ddply(c("ID", "Year"), function(x) x %>% 
+                                mutate(start.monitoring = as.Date(min(.$Time)), 
+                                       end.monitoring = as.Date(max(.$Time)))) %>% 
+      subset(start.monitoring == start & end.monitoring == end) %>% 
+      mutate(ID_Year = as.factor(paste(ID, Year, sep ="_")))
+  }
+
   
   # calculate the dt between successive relocations for each individual and exclude 
   # those with less fixes per day than nfixes (e.g., 1 for parturition) and missing data for more than
   # dayloss (e.g., 3 for parturition) consecutive days
   
-  tempo3 <- tempo2 %>% 
+  tempo2 <- tempo %>% 
     ddply(c("ID", "Year"), 
           function(x) x %>% 
             arrange(Time) %>% 
@@ -64,13 +66,13 @@ prepData <- function(df, start, end, nfixes = Inf, dayloss = Inf){
   cat(paste0("Period clipped to ", start," - ", end, "\n"))
   cat(paste0("Number of excluded individuals-years: ",
              length(unique(paste0(df$ID,df$Year)))-
-               length(unique(paste0(tempo3$ID,tempo3$Year))),
+               length(unique(paste0(tempo2$ID,tempo2$Year))),
              "\n"))
   
   if(class(df)[1] == "sf"){
-    tempo3 <- st_as_sf(tempo3, crs = st_crs(df))
+    tempo2 <- st_as_sf(tempo2, crs = st_crs(df))
   }
-  return(tempo3)
+  return(tempo2)
 }
 
 
