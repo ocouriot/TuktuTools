66ad47d9194a94bf5974e0455676fc4d72b77fa6
Elie Gurarie <egurarie@umd.edu>
2023-05-08 06:23:12 -0400

    Pushing crazy Tuktutools updates

--- R/getspeed.R
+++ R/getspeed.R
@@ -20,20 +20,24 @@
 #' @export
 #' 
 
-getSpeed <- function(x){
+getSpeed <- function(x, id.col = "ID", x.col = "x", y.col = "y", ...){
   
   if(class(x)[1] == "data.frame"){
-    df_annoted <- x %>% as.data.frame %>% ddply(c("ID"), annotateWithSpeeds)
+    df_annotated <- x %>% mutate(X = get(x.col), Y = get(y.col)) %>%
+      ddply(c(id.col), annotateWithSpeeds, ...)
   }
   if(class(x)[1] == "sf"){
     CRS = st_crs(x)
-    df_annoted <- x %>% as.data.frame %>% ddply(c("ID"), annotateWithSpeeds) %>% st_as_sf(crs = CRS)
+    x$x <- st_coordinates(x)[,1]
+    x$y <- st_coordinates(x)[,2]
+    df_annotated <- x %>% as.data.frame %>% ddply(c(id.col), annotateWithSpeeds, ...) %>% 
+      st_as_sf(crs = CRS)
   }
-  return(df_annoted)
+  return(df_annotated)
 }
 
-annotateWithSpeeds <- function(df){
-  droplevels(df) %>% dplyr::arrange(Time) %>% 
+annotateWithSpeeds <- function(df, time.col = "Time"){
+  droplevels(df) %>% mutate(Time = get(time.col)) %>% dplyr::arrange(Time) %>% 
     mutate(z = x+1i*y, 
            dt = c(NA, difftime(Time[-1], Time[-length(Time)], units = "hours") %>% as.numeric), 
            sl = c(NA, Mod(diff(z))), 
